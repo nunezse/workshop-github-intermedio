f60dc89e5245f6f46320228a57457c6b14d1e2cf
nunezse <nunezse@bccr.fi.cr>
2026-05-11 11:16:46 -0600

    Overload Add method to accept integer parameters

--- src/FinancialUtils/Calculator.cs
+++ src/FinancialUtils/Calculator.cs
@@ -10,6 +10,8 @@ public static class Calculator
     /// </summary>
     public static decimal Add(decimal a, decimal b) => a + b;
 
+    public static decimal Add(int a, int b) => a + b;
+
     /// <summary>
     /// Resta b de a.
     /// </summary>
